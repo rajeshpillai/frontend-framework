58f77a88b6b72c7c3757ec8c4055c69845a9ae51
Rajesh Pillai <pillai.rajesh@gmail.com>
2017-04-05 07:17:16 +0530

    step -1 of frontend framework

---
+++ app.js
@@ -0,0 +1,12 @@
+var posts = [
+  {
+    permalink: 'introducing-front',
+    title: '10 reasons Front is the best framework ever',
+    content: "<ol><li>Front is really the best of the bests, no doubts about it.</li><li>Have you used it? Then you don't know.</li><li>It's super incredibly amazing. Like double what you'd expect from a framework but even better, at the same time.</li><li>Try it now... Please!</li><li>Not sure I can list 10 reasons actually...</li><li>It's good.</li><li>No bugs.</li><li>OK, I give up.</li></ol>"
+  },
+  {
+    permalink: 'how-to-use-front',
+    title: 'How to use the Front framework like a boss',
+    content: '<p>Just RTFM already.</p><p><em>(TODO write manual)</em></p>'
+  },
+]
